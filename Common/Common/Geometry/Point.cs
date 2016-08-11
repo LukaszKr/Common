55137c94ee77a12d1ce4b2f09b01f6027120b972
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2016-08-11 20:35:37 +0200

    geometry module stump

---
+++ Common/Common/Geometry/Point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Common.Geometry
+{
+	public class Point
+    {
+		public float X { get; private set; }
+		public float Y { get; private set; }
+
+		public Point(float x, float y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public Point(Point point)
+		{
+			Copy(point);
+		}
+
+		public void Copy(Point point)
+		{
+			X = point.X;
+			Y = point.Y;
+		}
+
+		public double Distance(Point point)
+		{
+			float distanceX = point.X-X;
+			float distanceY = point.Y-Y;
+			distanceX = distanceX*distanceX;
+			distanceY = distanceY*distanceY;
+			return distanceX+distanceY;
+		}
+
+		public double DistanceSqrt(Point point)
+		{
+			return Math.Sqrt(Distance(point));
+		}
+	}
+}
