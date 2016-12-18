eeca73e68b7ee30fd6cc1e0484974d0d17a1efe5
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2016-12-18 13:51:25 +0100

    bugfix for json tokenizer

--- Common/Parsing/JSON/JSONTokenizer.cs
+++ Common/Parsing/JSON/JSONTokenizer.cs
@@ -2,7 +2,7 @@
 {
 	public class JSONTokenizer: Tokenizer
 	{
-		private string[] m_Separators = new string[]
+		private static string[] m_Separators = new string[]
 		{
 			JsonConst.ARRAY_CLOSE, JsonConst.ARRAY_OPEN,
 			JsonConst.BRACKETS_CLOSE, JsonConst.BRACKETS_OPEN,
@@ -10,12 +10,12 @@
 			JsonConst.QUOTATION, JsonConst.ESCAPED_QUOTATION
 		};
 
-		private string[] m_Quoted = new string[]
+		private static string[] m_Quoted = new string[]
 		{
 			JsonConst.QUOTATION
 		};
 
-		private string[] m_Value = new string[]
+		private static string[] m_Value = new string[]
 		{
 			JsonConst.QUOTATION,
 			JsonConst.SEPARATOR
@@ -30,8 +30,8 @@
 		{
 			switch(token.Value)
 			{
-				case JsonConst.KEY_VALUE_SEPARATOR:
-					return m_Value;
+				//case JsonConst.KEY_VALUE_SEPARATOR:
+				//	return m_Value;
 				//case JsonConst.QUOTATION:
 				//	return m_Quoted;
 				default:
