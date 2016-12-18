6a4005653f7bc2a59a711d665737dc2df46df945
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2016-12-18 15:33:27 +0100

    additional enum

---
+++ Common/Enums/EDirection.cs
@@ -0,0 +1,22 @@
+﻿namespace ProceduralLevel.Common
+{
+	public enum EDirection
+    {
+		Up = 0, 
+		Right = 1,
+		Down = 2,
+		Left = 3
+    }
+
+	public enum EHorizontalDirection
+	{
+		Right = 1,
+		Left = 3
+	}
+
+	public enum EVerticalDirection
+	{
+		Up = 0,
+		Down = 2
+	}
+}
