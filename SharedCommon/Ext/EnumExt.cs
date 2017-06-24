81f1d7e26786308b1f49fb0910ba9474154690fd
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2017-06-24 13:40:28 +0200

    enum ext

---
+++ SharedCommon/Ext/EnumExt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProceduralLevel.Common.Ext
+{
+	public static class EnumExt
+    {
+		public static void ValueRange<EnumType>(out int minValue, out int maxValue)
+		{
+			EnumType[] values = (EnumType[])Enum.GetValues(typeof(EnumType));
+			maxValue = int.MinValue;
+			minValue = int.MaxValue;
+			for(int x = 0; x < values.Length; x++)
+			{
+				EnumType value = values[x];
+				int eValue = value.GetHashCode();
+				maxValue = Math.Max(eValue, maxValue);
+				minValue = Math.Min(eValue, minValue);
+			}
+		}
+
+		public static int MinValue<EnumType>()
+		{
+			int minValue, maxValue;
+			ValueRange<EnumType>(out minValue, out maxValue);
+			return minValue;
+		}
+
+		public static int MaxValue<EnumType>()
+		{
+			int minValue, maxValue;
+			ValueRange<EnumType>(out minValue, out maxValue);
+			return maxValue;
+		}
+	}
+}
