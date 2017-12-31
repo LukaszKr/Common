d09bc3fd77ce5d3c79e198d97bb275d78de6ff5f
Lukasz <lukasz.kra@outlook.com>
2017-12-31 12:09:04 +0100

    ECS moved to separate project

--- Projects/Serialization/Serialization/Serializable/IArraySerializable.cs
+++ Projects/Serialization/Serialization/Serializable/IArraySerializable.cs
@@ -1,8 +1,8 @@
 ﻿namespace ProceduralLevel.Serialization
 {
 	public interface IArraySerializable
-    {
+	{
 		void Serialize(AArray serializer);
 		void Deserialize(AArray serializer);
-    }
+	}
 }
