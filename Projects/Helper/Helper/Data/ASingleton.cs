c28aa77dbedaefb1aaac32266b8bf516a940fb46
Lukasz <lukasz.kra@outlook.com>
2018-05-13 20:49:53 +0200

    simple singleton class

---
+++ Projects/Helper/Helper/Data/ASingleton.cs
@@ -0,0 +1,12 @@
+﻿namespace ProceduralLevel.Common.Helper
+{
+	public abstract class ASingleton<TType> where TType: class, new()
+	{
+		public readonly TType Instance = new TType();
+
+		protected ASingleton()
+		{
+
+		}
+	}
+}
