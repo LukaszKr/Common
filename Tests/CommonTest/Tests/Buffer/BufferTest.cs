cdf54b1ee7f3b38801263c414ebbecd3c2252c03
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2019-09-15 11:57:42 +0200

    few more buffer tests

--- Tests/CommonTest/Tests/Buffer/BufferTest.cs
+++ Tests/CommonTest/Tests/Buffer/BufferTest.cs
@@ -15,12 +15,38 @@ namespace ProceduralLevel.Common.Tests.Buffer
 		}
 
 		[Test]
+		public void Bool()
+		{
+			m_Buffer.Write(true);
+			m_Buffer.Write(false);
+			Assert.AreEqual(2, m_Buffer.Length);
+			Assert.IsTrue(m_Buffer.ReadBool());
+			Assert.IsFalse(m_Buffer.ReadBool());
+			AssertPostTest();
+		}
+
+		[Test]
+		public void Byte()
+		{
+			byte val = 4;
+			m_Buffer.Write(val);
+			Assert.AreEqual(1, m_Buffer.Length);
+			Assert.AreEqual(val, m_Buffer.ReadByte());
+			AssertPostTest();
+		}
+
+		[Test]
 		public void String()
 		{
 			string str = "Hello World";
 			m_Buffer.Write(str);
 			Assert.AreEqual(str.Length+4, m_Buffer.Length); //str length + str content
 			Assert.AreEqual(str, m_Buffer.ReadString());
+			AssertPostTest();
+		}
+
+		private void AssertPostTest()
+		{
 			Assert.AreEqual(0, m_Buffer.UnreadCount);
 		}
 	}
