599e236d448bf87c39806267208e9fa60194058e
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2020-12-29 12:09:33 +0100

    direction flags

---
+++ Projects/Common/Common/Grid/2D/Enum/EDirectionFlag2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProceduralLevel.Common.Grid
+{
+	[Flags]
+	public enum EDirectionFlag2D: byte
+	{
+		Up = 1 << 0,
+		Down = 1 << 1,
+		Left = 1 << 2,
+		Right = 1 << 3,
+
+		All = Up | Down | Left | Right
+	}
+
+	public static class EDirectionFlag2DExt
+	{
+		public readonly static EDirectionFlag2D[] Values = (EDirectionFlag2D[])Enum.GetValues(typeof(EDirectionFlag2D));
+
+		public static bool Contains(this EDirectionFlag2D flag, EDirectionFlag2D other)
+		{
+			return (flag & other) == other;
+		}
+	}
+}
---
+++ Projects/Common/Common/Grid/3D/Enum/EDirectionFlag3D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProceduralLevel.Common.Grid
+{
+	[Flags]
+	public enum EDirectionFlag3D: byte
+	{
+		Up = 1 << 0,
+		Down = 1 << 1,
+		Left = 1 << 2,
+		Right = 1 << 3,
+		Front = 1 << 4,
+		Back = 1 << 5,
+
+		All = Up | Down | Left | Right | Front | Back
+	}
+
+	public static class EDirectionFlag3DExt
+	{
+		public readonly static EDirectionFlag3D[] Values = (EDirectionFlag3D[])Enum.GetValues(typeof(EDirectionFlag3D));
+
+		public static bool Contains(this EDirectionFlag3D flag, EDirectionFlag3D other)
+		{
+			return (flag & other) == other;
+		}
+	}
+}
