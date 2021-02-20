3a4c92f7c0d7fb71cb783b5b6ca2fa0adffd61ca
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2021-02-20 19:43:49 +0100

    buffer method to write only part of array/list

--- Projects/Common/Common/Buffer/Writer/BinaryBufferWriter.Types.cs
+++ Projects/Common/Common/Buffer/Writer/BinaryBufferWriter.Types.cs
@@ -7,22 +7,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Char
 		public BinaryBufferWriter Write(char[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<char> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(char[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<char> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -31,22 +39,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Bool
 		public BinaryBufferWriter Write(bool[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<bool> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(bool[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<bool> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -55,22 +71,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Byte
 		public BinaryBufferWriter Write(byte[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<byte> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(byte[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<byte> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -79,22 +103,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Short
 		public BinaryBufferWriter Write(short[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<short> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(short[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<short> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -103,22 +135,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region UShort
 		public BinaryBufferWriter Write(ushort[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<ushort> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(ushort[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<ushort> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -127,22 +167,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Int
 		public BinaryBufferWriter Write(int[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<int> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(int[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<int> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -151,22 +199,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region UInt
 		public BinaryBufferWriter Write(uint[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<uint> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(uint[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<uint> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -175,22 +231,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Long
 		public BinaryBufferWriter Write(long[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<long> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(long[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<long> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -199,22 +263,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region ULong
 		public BinaryBufferWriter Write(ulong[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<ulong> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(ulong[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<ulong> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -223,22 +295,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region String
 		public BinaryBufferWriter Write(string[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<string> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(string[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<string> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -247,22 +327,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Float
 		public BinaryBufferWriter Write(float[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<float> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(float[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<float> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
@@ -271,22 +359,30 @@ namespace ProceduralLevel.Common.Buffer
 		#region Double
 		public BinaryBufferWriter Write(double[] arr)
 		{
-			int length = arr.Length;
-			Write(length);
-			for(int x = 0; x < length; ++x)
-			{
-				Write(arr[x]);
-			}
-			return this;
+			return Write(arr, 0, arr.Length);
 		}
 
 		public BinaryBufferWriter Write(List<double> list)
 		{
-			int count = list.Count;
+			return Write(list, 0, list.Count);
+		}
+
+		public BinaryBufferWriter Write(double[] arr, int offset, int length)
+		{
+			Write(length);
+			for(int x = 0; x < length; ++x)
+			{
+				Write(arr[offset+x]);
+			}
+			return this;
+		}
+
+		public BinaryBufferWriter Write(List<double> list, int offset, int count)
+		{
 			Write(count);
 			for(int x = 0; x < count; ++x)
 			{
-				Write(list[x]);
+				Write(list[offset+x]);
 			}
 			return this;
 		}
