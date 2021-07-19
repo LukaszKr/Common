e7d35372d6afb4248d60bf9c92815c8d4c214935
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2021-07-19 22:13:14 +0200

    ReadonlyEvent interface

---
+++ Projects/Common/Common/Event/Interface/IReadonlyEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProceduralLevel.Common.Event
+{
+	public interface IReadonlyEvent<TCallback>
+		where TCallback: Delegate
+	{
+		void AddListener(TCallback callback);
+		void RemoveListener(TCallback callback);
+	}
+}
