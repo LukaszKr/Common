70a0be247ea6dec3a0081b4288c0250b58587a2f
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2022-04-04 12:34:02 +0200

    context class

---
+++ Projects/Common/Common/Context/AContextClass.cs
@@ -0,0 +1,49 @@
+﻿using ProceduralLevel.Common.Event;
+
+namespace ProceduralLevel.Common.Context
+{
+	public abstract class AContextClass<TContext>
+		where TContext : class
+	{
+		protected TContext m_Context;
+		private readonly EventBinder m_ContextBinder = new EventBinder();
+
+		public TContext Context { get { return m_Context; } }
+
+		public void SetContext(TContext newContext)
+		{
+			if(newContext == m_Context)
+			{
+				return;
+			}
+
+			m_ContextBinder.UnbindAll();
+			TContext oldContext = m_Context;
+			m_Context = newContext;
+			if(newContext != null)
+			{
+				if(oldContext != null)
+				{
+					OnReplace(m_ContextBinder, oldContext);
+				}
+				else
+				{
+					OnAttach(m_ContextBinder);
+				}
+			}
+			else
+			{
+				OnDetach();
+			}
+		}
+
+		protected virtual void OnReplace(EventBinder binder, TContext oldContext)
+		{
+			OnDetach();
+			OnAttach(binder);
+		}
+
+		protected abstract void OnAttach(EventBinder binder);
+		protected abstract void OnDetach();
+	}
+}
---
+++ Projects/Common/Common/Context/ContextClass.cs
@@ -0,0 +1,45 @@
+﻿using ProceduralLevel.Common.Event;
+
+namespace ProceduralLevel.Common.Context
+{
+	public class ContextClass<TContext> : AContextClass<TContext>
+		where TContext : class
+	{
+		private readonly OnDetachDelegate m_OnDetach;
+		private readonly OnAttachDelegate m_OnAttach;
+		private readonly OnReplaceDelegate m_OnReplace;
+
+		public delegate void OnDetachDelegate();
+		public delegate void OnAttachDelegate(EventBinder binder);
+		public delegate void OnReplaceDelegate(EventBinder binder, TContext oldContext);
+
+		public ContextClass(OnAttachDelegate onAttach, OnDetachDelegate onDetach, OnReplaceDelegate onReplace = null)
+		{
+			m_OnAttach = onAttach;
+			m_OnDetach = onDetach;
+			m_OnReplace = onReplace;
+		}
+
+		protected override void OnAttach(EventBinder binder)
+		{
+			m_OnAttach(binder);
+		}
+
+		protected override void OnDetach()
+		{
+			m_OnDetach();
+		}
+
+		protected override void OnReplace(EventBinder binder, TContext oldContext)
+		{
+			if(m_OnReplace != null)
+			{
+				m_OnReplace(binder, oldContext);
+			}
+			else
+			{
+				base.OnReplace(binder, oldContext);
+			}
+		}
+	}
+}
