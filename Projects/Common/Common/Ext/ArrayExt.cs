94bca290186f475b886c1e5e79f66dabd93c99f8
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-11-05 16:18:10 +0100

    2d and 3d array resize

--- Projects/Common/Common/Ext/ArrayExt.cs
+++ Projects/Common/Common/Ext/ArrayExt.cs
@@ -42,16 +42,76 @@ namespace ProceduralLevel.Common.Ext
 
 		public static TData[] Resize<TData>(this TData[] array, int length)
 		{
+			TData[] newArray = new TData[length];
 			if(array == null)
 			{
 				return new TData[length];
 			}
 			int oldLength = array.Length;
 			int iter = Math.Min(length, oldLength);
-			TData[] newArray = new TData[length];
 			for(int x = 0; x < iter; ++x)
 			{
 				newArray[x] = array[x];
+			}
+			return newArray;
+		}
+
+		public static TData[][] Resize<TData>(this TData[][] array, GridSize2D size)
+		{
+			return array.Resize(size.X, size.Y);
+		}
+
+		public static TData[][] Resize<TData>(this TData[][] array, int sizeX, int sizeY)
+		{
+			TData[][] newArray = Create<TData>(sizeX, sizeY);
+			if(array == null)
+			{
+				return newArray;
+			}
+
+			int minSizex = Math.Min(sizeX, array.Length);
+			for(int x = 0; x < minSizex; ++x)
+			{
+				TData[] xAxisNew = newArray[x];
+				TData[] xAxis = array[x];
+				int minSizeY = Math.Min(xAxisNew.Length, xAxis.Length);
+				for(int y = 0; y < minSizeY; ++y)
+				{
+					xAxisNew[y] = xAxis[y];
+				}
+			}
+			return newArray;
+		}
+
+		public static TData[][][] Resize<TData>(this TData[][][] array, GridSize3D size)
+		{
+			return array.Resize(size.X, size.Y, size.Z);
+		}
+
+		public static TData[][][] Resize<TData>(this TData[][][] array, int sizeX, int sizeY, int sizeZ)
+		{
+			TData[][][] newArray = Create<TData>(sizeX, sizeY, sizeZ);
+			if(array == null)
+			{
+				return newArray;
+			}
+
+			int minSizeX = Math.Min(sizeX, array.Length);
+			for(int x = 0; x < minSizeX; ++x)
+			{
+				TData[][] xAxisNew = newArray[x];
+				TData[][] xAxis = array[x];
+				int minSizeY = Math.Min(xAxisNew.Length, xAxis.Length);
+				for(int y = 0; y < minSizeY; ++y)
+				{
+					TData[] yAxisNew = xAxisNew[y];
+					TData[] yAxis = xAxis[y];
+					int minSizeZ = Math.Min(yAxisNew.Length, yAxis.Length);
+					for(int z = 0; z < minSizeZ; ++z)
+					{
+						yAxisNew[y] = yAxis[y];
+					}
+				}
 			}
 			return newArray;
 		}
